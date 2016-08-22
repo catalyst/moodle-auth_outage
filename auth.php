1b7610c3b386d6256353578fea4847c4f057e5db
Marcus Boon <(none)>
2016-08-22 16:25:11 +1000

    WR#259574: Initial auth_outage class structure

---
+++ auth.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This plugin allows for an outage window to be configured
+ * and then optionally allows only a subset of IPs to connect,
+ * it also shows an outage notification to users.
+ *
+ * @package     auth
+ * @subpackage  outage
+ * @author      Marcus Boon<marcus@catalyst-au.net>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU Public License
+ */
+
+if (!defined('MOODLE_INTERNAL')) {
+    die('Direct access to this script is forbidden.'); // It must be included from a Moodle page.
+}
+
+require_once($CFG->libdir.'/authlib.php');
+
+class auth_plugin_outage extends auth_plugin_base {
+
+    public function __construct() {
+
+        $this->authtype = 'outage';
+        $this->roleauth = 'auth_outage';
+        $this->component = 'auth_outage';
+        $this->errorlogtag = '[AUTH_OUTAGE]';
+        $this->config = get_config('auth_outage');
+
+        // Set to defaults if undefined.
+        $this->config = $this->set_defaults($this->config);
+    }
+}

e1b6279437ca6632e3c6a65106ca608877cae027
Nathan Nguyen <nathannguyen@catalyst-au.net>
2018-12-06 15:07:58 +1100

    user date

--- views/warningbar/warningbar.js
+++ views/warningbar/warningbar.js
@@ -114,8 +114,10 @@ var authOutageWarningBar = {
     seconds2hms: function(seconds) {
         var minutes = Math.floor(seconds / 60);
         var hours = Math.floor(minutes / 60);
+        var days = Math.floor(hours / 24);
         seconds %= 60;
         minutes %= 60;
+        hours %= 24;
         // Cross-browser simple solution for padding zeroes.
         if (minutes < 10) {
             minutes = "0" + minutes;
@@ -123,6 +125,6 @@ var authOutageWarningBar = {
         if (seconds < 10) {
             seconds = "0" + seconds;
         }
-        return hours + ':' + minutes + ':' + seconds;
+        return days + ' day(s) ' + hours + ':' + minutes + ':' + seconds;
     }
 };

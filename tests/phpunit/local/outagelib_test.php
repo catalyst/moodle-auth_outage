777e466a0ad5e3c22eb63720d30c25fbee2c8d61
Peter Burnett <peterburnett@catalyst-au.net>
2020-03-25 15:15:02 +1000

    Issue #189: Updated unit tests

--- tests/phpunit/local/outagelib_test.php
+++ tests/phpunit/local/outagelib_test.php
@@ -289,6 +289,7 @@ class outagelib_test extends advanced_testcase {
 if ((time() >= 123) && (time() < 456)) {
     define('MOODLE_INTERNAL', true);
     require_once($CFG->dirroot.'/lib/moodlelib.php');
+    require_once($CFG->dirroot.'/lib/classes/ip_utils.php');
     if (!remoteip_in_list('hey\'\"you
 a.b.c.d
 e.e.e.e/20')) {
@@ -330,6 +331,7 @@ EOT;
 if ((time() >= 123) && (time() < 456)) {
     define('MOODLE_INTERNAL', true);
     require_once($CFG->dirroot.'/lib/moodlelib.php');
+    require_once($CFG->dirroot.'/lib/classes/ip_utils.php');
     if (!remoteip_in_list('127.0.0.1')) {
         header($_SERVER['SERVER_PROTOCOL'] . ' 503 Moodle under maintenance');
         header('Status: 503 Moodle under maintenance');

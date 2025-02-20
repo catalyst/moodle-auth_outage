99452cb4f3302c38f74874f35f066f15d7c63640
Sasha Anastasi <sasha.anastasi@catalyst.net.nz>
2025-02-20 13:01:11 +1300

    outagelib: adjust exception trigger for phpunit10

--- classes/local/outagelib.php
+++ classes/local/outagelib.php
@@ -266,7 +266,9 @@ class outagelib {
 
         // Used to test the try block in case of errors.
         if (PHPUNIT_TEST && optional_param('auth_outage_break_code', false, PARAM_INT)) {
-            (new stdClass())->invalidfield; // Triggers an exception.
+            if (!isset($emptyvar)) {
+                throw new coding_exception('Empty variable "emptyvar" is empty');
+            }
         }
 
         // Nothing preventing the injection.

c44f168cfc1438af551b635fe23274b0f1bba670
guillaumebarat <guillaumebarat@catalyst-au.net>
2025-12-03 15:16:23 +1000

    fix error debug warning

--- edit.php
+++ edit.php
@@ -48,7 +48,11 @@ if ($mform->is_cancelled()) {
 
 $clone = optional_param('clone', 0, PARAM_INT);
 $edit = optional_param('edit', 0, PARAM_INT);
-$start = optional_param_array('starttime', [], PARAM_INT);
+if (array_key_exists('starttime', $_POST) && is_array($_POST['starttime'])) {
+    $start = optional_param_array('starttime', [], PARAM_INT);
+} else {
+    $start = optional_param('starttime', 0, PARAM_INT);
+}
 if (!empty($start['year']) && !empty($start['month']) && !empty($start['day'])) {
     $hour = $start['hour'] ?? 0;
     $minute = $start['minute'] ?? 0;

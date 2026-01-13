a3721d492e9ae8fe6bc19f59c61cd393ffd37d33
Jay Oswald <jayoswald@catalyst-au.net>
2026-01-13 17:51:21 +1100

    retrigger

--- version.php
+++ version.php
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-// Dummy change to trigger CI 28
+// Dummy change to trigger CI 29
 
 /**
  * Version information.

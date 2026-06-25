be740fd13937531f4ab4e81844b97a07bbecfe23
Sarah Cotton <sarah.cotton@catalyst-eu.net>
2026-06-25 13:44:10 +0100

    WR489688: Security fix #30

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = "auth_outage";
-$plugin->version = 2024081905;                  // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release = 2024081905;                  // Human-readable release information.
+$plugin->version = 2024081906;                  // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release = 2024081906;                  // Human-readable release information.
 $plugin->requires = 2017111309;                 // 2017111309 = T13, but this really requires 3.9 and higher.
 $plugin->maturity = MATURITY_STABLE;            // Suitable for PRODUCTION environments!
 $plugin->supported = [39, 405];                 // A range of branch numbers of supported moodle versions.
